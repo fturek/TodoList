090ed3fee7ae062b1011678e0be77b39299eaacd
fturek <filippturek@gmail.com>
2020-11-10 22:42:27 +0100

    Get rid off TodoItemResultDiffCallback

--- app/src/main/java/com/fturek/todolist/data/model/TodoItem.kt
+++ app/src/main/java/com/fturek/todolist/data/model/TodoItem.kt
@@ -1,6 +1,5 @@
 package com.fturek.todolist.data.model
 
-import androidx.recyclerview.widget.DiffUtil
 import com.google.firebase.Timestamp
 
 data class TodoItem(
@@ -9,17 +8,3 @@ data class TodoItem(
     var createdAt: Timestamp? = null,
     var iconUrl: String? = null
 )
-
-class TodoItemResultDiffCallback : DiffUtil.ItemCallback<TodoItem>() {
-
-    override fun areItemsTheSame(oldItem: TodoItem, newItem: TodoItem): Boolean {
-        return oldItem == newItem
-    }
-
-    override fun areContentsTheSame(oldItem: TodoItem, newItem: TodoItem): Boolean {
-        return oldItem.title == newItem.title &&
-                oldItem.description == newItem.description &&
-                oldItem.createdAt == newItem.createdAt &&
-                oldItem.iconUrl == newItem.iconUrl
-    }
-}
